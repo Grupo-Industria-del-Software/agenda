95a564abc5aa847161dd877d06d1911ef7d743ec
José Alberto Ulloa Avila <ackertaker69@gmail.com>
2025-03-17 00:12:29 -0600

    feat: Logica y cambios en Activity

--- Agenda/Domain/AggregateRoots/Activities/Activity.cs
+++ Agenda/Domain/AggregateRoots/Activities/Activity.cs
@@ -6,15 +6,15 @@ namespace Domain.AggregateRoots.Activities
 {
     public class Activity : AggregateRoot
     {
-        public DateTime StartDateTime { get; set; }
-        public DateTime EndDateTime { get; set; }
+        public DateTime? StartDateTime { get; set; }
+        public DateTime? EndDateTime { get; set; }
         public int UserId { get; set; }
         public User? User { get; set; }
         public int TypeId { get; set; }
         public Type? Type { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public bool Finished { get; set; }
+        public bool Finished { get; set; } = false;
         public List<Task> Tasks { get; set; } = new();
     }
 }
